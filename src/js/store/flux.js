6432ea17399da76ac9c4e8eb651d610c82f6fe9d
kparra7491 <kparra7491@gmail.com>
2021-01-06 14:32:09 +0000

    updates

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,18 +12,42 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			list: [],
+			input: ""
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
+			loadInitialData: () => {
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+                */
+
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/kparra")
+					.then(function(response) {
+						if (!response.ok) {
+							throw Error(response.statusText);
+							console.log("error after response");
+						}
+						return response.json();
+					})
+					.then(jsonifiedResponse => {
+						setStore({
+							list: jsonifiedResponse
+						});
+					})
+
+					.catch(function(error) {
+						console.log("ya broke it", error);
+					});
 			},
+			//Get a list for a user
+			//New List (if no todos)
+			//update the list
+			//Delete a list
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
@@ -37,7 +61,65 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+
+			listAdd: newTask => {
+				//add 'input' to the list
+
+				//get the store
+				const store = getStore();
+
+				//we have to loop the entire demo array to look for the respective index
+				//and change its color
+
+				if (newTask) {
+					var newListObject = { label: newTask, done: false };
+					var updatedList = store.list.concat(newListObject);
+					setStore({
+						list: updatedList
+					});
+
+					fetch("https://assets.breatheco.de/apis/fake/todos/user/kparra", {
+						method: "PUT", // or 'POST'
+						body: JSON.stringify(store.list), // data can be `string` or {object}!
+						headers: {
+							"Content-Type": "application/json"
+						}
+					})
+						.then(res => res.json())
+						.then(response => console.log("Success:", JSON.stringify(response)))
+						.catch(error => console.error("Error:", error));
+				}
+
+				//reset the global store
+			},
+			finish: index => {
+				//get the store
+				const store = getStore();
+				console.log(index);
+				//we have to loop the entire demo array to look for the respective index
+				//and change its color
+
+				var newList = store.list;
+				var updatedList = newList.splice(index, 1);
+
+				setStore({
+					list: updatedList
+				});
+
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/kparra", {
+					method: "PUT", // or 'POST'
+					body: JSON.stringify(store.list), // data can be `string` or {object}!
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(res => res.json())
+					.then(response => console.log("Success:", JSON.stringify(response)))
+					.catch(error => console.error("Error:", error));
 			}
+
+			//reset the global store
 		}
 	};
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	const [task, setTask] = useState("");
+
+	return (
+		<div>
+			<input
+				type="text"
+				className="input"
+				id="in"
+				value={task}
+				onChange={e => setTask(e.target.value)}
+				onKeyUp={e => {
+					if (e.keyCode == 13) {
+						actions.listAdd(task);
+						setTask("");
+					}
+				}}
+			/>
+
+			<div />
+
+			<ol>
+				{store.list.map((item, i) => {
+					return (
+						<li key={i}>
+							<div>{item.label}</div>
+							<div>{item.done ? "Done!" : "Pending"}</div>
+							<div onClick={i => actions.finish(i)}>finish task</div>
+						</li>
+					);
+				})}
+			</ol>
+		</div>
+	);
+};
